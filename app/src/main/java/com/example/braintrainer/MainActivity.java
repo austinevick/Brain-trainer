28fbb416e425401617da480f60a108eaebee909e
Augustine Victor <augustinevickky@gmail.com>
2020-12-09 04:06:44 +0100

    first commit

---
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button goButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    goButton = findViewById(R.id.goButton);
+    }
+
+    public void start(View view) {
+
+    }
+}
